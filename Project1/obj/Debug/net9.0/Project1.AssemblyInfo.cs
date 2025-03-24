5b8df09f3c228214c370a25058b15068d605f20d
fepeti13 <fepeti13@gmail.com>
2025-03-24 17:07:32 +0200

    bindbuffer bufferdata felcserelve

--- Project1/obj/Debug/net9.0/Project1.AssemblyInfo.cs
+++ Project1/obj/Debug/net9.0/Project1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16c9891a0b696107c0db845b72df329c411d1fce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22d2802cfc46235d7985d41b78cdeca8c14053e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
